ea01ea7c4c3f85ae4bc2bc3f10a46f213509ea37
Ashish <8013785+ashu-22@users.noreply.github.com>
2017-09-04 01:11:34 +0530

    index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+ var cmd_map = {
+                    'init': './src/cmd_init',
+                    'task': './src/cmd_task'
+                    'task': './src/cmd_task',
+                    'addme': './src/cmd_addme',
+               };
+  function main (args){
